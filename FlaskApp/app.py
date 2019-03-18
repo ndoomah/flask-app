a857fcba495876119dfc0edde2fe37dd46f876ed
ndoomah <nabiiha.doomah@umail.uom.ac.mu>
2019-03-18 18:28:54 +0400

    updates

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -168,6 +168,6 @@ def notif_connect():
 def test_message():
     socketio.emit('newdata', {'lat': '-20.2233014','lng': '57.5382649'}, namespace='/test')'''
 
-if __name__ == "__main__":
-    socketio.run(app)
+#if __name__ == "__main__":
+socketio.run(app)
     #app.run()
