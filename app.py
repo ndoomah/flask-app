68f2a9459410e50208814b50c30148341210f415
ndoomah <nabiiha.doomah@umail.uom.ac.mu>
2019-03-24 19:56:40 +0400

    fixes

--- app.py
+++ app.py
@@ -169,8 +169,7 @@ def test_message():
     socketio.emit('newdata', {'lat': '-20.2233014','lng': '57.5382649'}, namespace='/test')'''
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT'))
-    print(port)
+    port = int(os.environ.get('PORT', 5000))
     socketio.run(app, host='0.0.0.0', port=port, resource="socket.io", policy_server=False)
     #socketio.run(app)
     #app.run()
